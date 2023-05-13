67d03c6e03af035b169d27f0562d6e0a34ef9715
lokesh jegini <jegini.lokesh@integra-india.co.in>
2023-05-13 19:14:28 +0530

    name-arrtibute-uses

--- 13-04-2023(forms-variables)/name-attribute/index1.js
+++ 13-04-2023(forms-variables)/name-attribute/index1.js
@@ -1,22 +1,8 @@
-function loki(event) {
+var form1 = document.querySelector(".form");
+form1.addEventListener("submit", (event) => {
   event.preventDefault();
+  const formData = new FormData(form1);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+});
 
-  var text1 = document.getElementById("text1").value;
-  var passord1 = document.getElementById("passord1").value;
-  var radio1 = document.getElementById("radio1").value;
-  var check1 = document.getElementById("check1").value;
-  var textarea1 = document.getElementById("textarea1").value;
-  console.log(text1);
-  console.log(passord1);
-  console.log(radio1);
-  console.log(check1);
-  console.log(textarea1);
-  document.getElementById("text1").value = "";
-  document.getElementById("passord1").value = "";
-  document.getElementById("radio1").value = "";
-  document.getElementById("check1").value = "";
-  document.getElementById("textarea1").value = "";
-
-  // Optionally, focus on the first text field after resetting the values
-  document.getElementById("text1").focus();
-}
