be6727d77207fc19b29c1b3cf7f983f253ff6891
lokesh jegini <jegini.lokesh@integra-india.co.in>
2023-05-15 22:56:51 +0530

    class-1 where-to start-to-understand-js-logic

--- 13-04-2023(forms-variables)/name-attribute/index1.js
+++ 13-04-2023(forms-variables)/name-attribute/index1.js
@@ -1,8 +1,46 @@
-var form1 = document.querySelector(".form");
-form1.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const formData = new FormData(form1);
-  const data = Object.fromEntries(formData);
-  console.log(data);
-});
+const todoInput = document.getElementById("todo-input");
+const addBtn = document.getElementById("add-btn");
+const todoList = document.getElementById("todo-list");
 
+addBtn.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteTodo);
+
+function addTodo() {
+  const task = todoInput.value;
+
+  if (task === "") {
+    return;
+  }
+
+  const todoItem = createTodoItem(task);
+  todoList.appendChild(todoItem);
+
+  todoInput.value = "";
+}
+
+function deleteTodo(event) {
+  const deleteBtn = event.target;
+  
+  if (deleteBtn.classList.contains("delete-btn")) {
+    const todoItem = deleteBtn.closest(".todo-item");
+    todoList.removeChild(todoItem);
+  }
+}
+
+function createTodoItem(task) {
+  const todoItem = document.createElement("li");
+  todoItem.classList.add("todo-item");
+
+  const taskElement = document.createElement("span");
+  taskElement.classList.add("task");
+  taskElement.textContent = task;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete-btn");
+  deleteBtn.textContent = "Delete";
+
+  todoItem.appendChild(taskElement);
+  todoItem.appendChild(deleteBtn);
+
+  return todoItem;
+}
