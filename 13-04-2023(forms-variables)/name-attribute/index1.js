afe2a89bc944caca358921acc8428dababadd602
lokesh jegini <jegini.lokesh@integra-india.co.in>
2023-05-16 23:04:10 +0530

    html-collection vs node-list

--- 13-04-2023(forms-variables)/name-attribute/index1.js
+++ 13-04-2023(forms-variables)/name-attribute/index1.js
@@ -1,23 +1,7 @@
-//before settime out
- var button1 = $('[role="menuitem"]');
-        console.log(button1[7]);
-        console.log(button1);
-
-//after settimout
-
-
-      setTimeout(() => {
-        var button1 = $('[role="menuitem"]');
-        console.log(button1[7]);
-        console.log(button1);
-      }, 200);
-      // setTimeout(alert("cbchcb"), 200);
-      setTimeout(() => {
-        debugger;
-        alert("cbchcb");
-      }, 200);
-
-      //widow.load()
-
-window.onload = function () { };
-
+var a = document.getElementById("loki");
+var b = document.getElementsByClassName("maurali");
+var c = document.getElementsByTagName("h3");
+var d = document.querySelector("#idloki");
+var e = document.querySelector(".classmurali");
+var f = document.querySelectorAll("#idloki1");
+var g = document.querySelectorAll(".classmurali");
