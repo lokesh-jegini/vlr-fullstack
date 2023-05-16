44c4c7af90ab1c12730bde0afa727266af97d82c
lokesh jegini <jegini.lokesh@integra-india.co.in>
2023-05-16 20:26:10 +0530

    why we use call back functions in js DOM

--- 13-04-2023(forms-variables)/name-attribute/index1.js
+++ 13-04-2023(forms-variables)/name-attribute/index1.js
@@ -1,16 +1,22 @@
-document.getElementById("demo").innerHTML = "Hello World!";
 // debugger;
-document.querySelectorAll("button")[0].onclick = () => {
-  // debugger;
-  // Create a new <div> element
-  const newDiv = document.createElement("div");
 
-  // Set some attributes and content for the new <div> element
-  newDiv.id = "myDiv";
-  newDiv.className = "myClass";
-  newDiv.textContent = "before and afte the graphically viw of dom!";
+document.querySelectorAll("button")[0].addEventListener("click", () => {
+  sum(10, 200);
+});
+// debugger;
+// Create a new <div> element
+const newDiv = document.createElement("div");
+// Set some attributes and content for the new <div> element
+newDiv.id = "myDiv";
+newDiv.className = "myClass";
+newDiv.textContent = "before and afte the graphically viw of dom!";
+// Append the new <div> element to an existing element in the document
+const parentElement = document.getElementById("demo");
+parentElement.appendChild(newDiv);
 
-  // Append the new <div> element to an existing element in the document
-  const parentElement = document.getElementById("demo");
-  parentElement.appendChild(newDiv);
-};
+function sum(a, b) {
+  debugger;
+  var a;
+  var b;
+  alert(a + b);
+}
